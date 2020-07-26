cb4dc53b1e6537035d89212d6887b697eaa3c4d0
unknown <r.tomaszewski@gmail.com>
2020-07-26 12:26:53 +0200

    Some changes in files index.js and package.json

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+const path = require("path")
 require("dotenv/config");
 
 
 const app=express();
 
+app.use(express.static(path.join(__dirname, './front-end/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
