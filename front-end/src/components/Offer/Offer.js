9d3f832384435b57cd48b0886380fd707af73bf1
unknown <r.tomaszewski@gmail.com>
2020-08-03 17:07:15 +0200

    Changed text and images in Offer

--- front-end/src/components/Offer/Offer.js
+++ front-end/src/components/Offer/Offer.js
@@ -6,21 +6,35 @@ function Offer(props)  {
         return (
             <div id="Offer">
                 <div className="offer-container">
-                    <div className="offer-container-text">
-                        <h3>Oferujemy :</h3>
-                        <ul>
-                            <li>- pranie bielizny hotelowej (pościele,ręczniki,szlafroki itp.)</li>
-                            <li>- tekstyliów dla restauracji i innych obiektów gastronomicznych</li>
-                            <li>- pranie odzieży roboczej</li>
-                            <li>- maglowanie</li>
-                            <li>- składanie i pakowanie</li>
-                        </ul>
-                        <h4>Zapraszamy klientów indywidualnych oraz firmy, hotele, pensjonaty, restauracje, sale bankietowe, domy weselne i wszystkich innych zainteresowanych.</h4>
-                        <h3>ZAPEWNIAMY TRANSPORT OD I DO KLIENTA !</h3>
-                        <h4>W celu uzyskania szczegółowych informacji na temat oferty, cen oraz ewentualnej współpracy prosimy o kontakt pod numerem telefonu <span>+48 573 355 294</span>, bądź za pomocą <span onClick={() => props.history.push("/contact")}>formularza kontaktowego</span></h4>
+                    <div className="offer-container-upper">
+                        <div className="offer-container-first-text offer-container-box">
+                            <div className="offer-container-first-text-wrapper">
+                                <h1>Zapraszamy</h1>
+                                <h3>obiekty noclegowe,</h3>
+                                <h3>gastronomiczne,</h3>
+                                <h3>firmy,</h3>
+                                <h3>klientów indywidualnych i wszystkich zainteresowanych.</h3>
+                            </div>
+                        </div>
+                        <div className="offer-container-first-image offer-container-box">
+                        <img src="./images/child.jpg" alt="" />
+                        </div>
                     </div>
-                    <div className="offer-container-image">
+                    <div className="offer-container-lower">
+                        <div className="offer-container-second-image offer-container-box">
                         <img src="./images/towel.jpg" alt="" />
+                        </div>
+                        <div className="offer-container-second-text offer-container-box">
+                        <div className="offer-container-second-text-wrapper">
+                        <h3>W celu uzyskania szczegółowych</h3>
+                            <h3>informacji na temat oferty,</h3>
+                            <h3>cen oraz współpracy</h3>
+                            <h3>prosimy o kontakt pod numerem telefonu</h3>
+                            <h3><span>+48 573 355 294</span></h3> 
+                            <h3>mailowo bądź za pomocą</h3>
+                            <h3><span onClick={() => props.history.push("/contact")}>formularza kontaktowego.</span></h3>
+                        </div>
+                        </div>
                     </div>
                 </div>
             </div>
